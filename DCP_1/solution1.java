46f101df19b975c8fccb934ba0628e63c62f5ef4
Vamsi Krishna <vamsikrishh0099@gmail.com>
2020-07-08 17:08:06 +0530

    Create solution1.java

---
+++ DCP_1/solution1.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+
+class Solution {
+    public boolean solve(int[] nums, int k) {
+        List<Integer> n1 = new ArrayList<Integer>();
+        int i = 0;
+        while(i < nums.length) {
+            if(n1.contains(k-nums[i]))
+                return true;
+            n1.add(nums[i]);
+            i++;
+        }
+        return false;
+    }
+}
