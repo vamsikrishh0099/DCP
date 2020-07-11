7196d7123a047f6326f0ff4e621da3de65ccd191
Vamsi Krishna <vamsikrishh0099@gmail.com>
2020-07-11 12:02:31 +0530

    Create solution4.java

---
+++ DCP_4/solution4.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+class DCP
+{
+	public int solve (int[] a)
+	{
+	   SortedSet<Integer> n=new TreeSet<Integer>();
+		for (int i=0;i<a.length;i++){
+		    n.add(a[i]);
+		}
+		for (int i=1;i<n.size();i++){
+		    if (!n.contains(i))
+		    return i; //if any missing positive integer,this is the answer.
+		}
+		return n.last()+1; //if all positive integers are present consequently,then answer is last integer + 1.
+	}
+}
